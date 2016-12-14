8f6abdffa6b361ac719669155218bc777a33eb9c
Demian Turner <demian@seagullsystems.com>
2016-12-14 17:58:24 +0800

    basics working

--- PollutionMonitor/AppDelegate.m
+++ PollutionMonitor/AppDelegate.m
@@ -12,96 +12,78 @@
 
 @interface AppDelegate()
 
-@property (weak, nonatomic) IBOutlet NSWindow *window;
+//@property (weak, nonatomic) IBOutlet NSWindow *window;
+//@property (strong, nonatomic) id popoverTransiencyMonitor;
 @property (strong, nonatomic) NSStatusItem *statusItem;
-//@property (strong, nonatomic) NSString *imageName;
-//@property (strong, nonatomic) NSPopover *popover;
-@property (strong, nonatomic) id popoverTransiencyMonitor;
 
 @end
 
 @implementation AppDelegate
 
--(void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-
-//    _imageName = @"white";
-//    self.statusItem = [self initializeStatusBarItem];
-//    
-//    _statusItem.image = [NSImage imageNamed:@"exclamation@2x.png"];
-//    NSString *portTitle = @"foo";
-//    NSString *quitTitle = @"Quit";
-//    _statusItem.menu = [self initializeStatusBarMenu:@{
-//                                                       portTitle: [NSValue valueWithPointer:nil],
-//                                                       quitTitle: [NSValue valueWithPointer:@selector(terminate:)]
-//                                                       }];
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    float oneHourInSeconds = 60.0 * 60.0;
+    [NSTimer scheduledTimerWithTimeInterval:oneHourInSeconds
+                                     target:self
+                                   selector:@selector(timerFired:)
+                                   userInfo:nil
+                                    repeats:YES];
     
-    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-//    self.statusItem.alternateImage = [NSImage imageNamed:@"white_alt@2x.png"];
-    self.statusItem.highlightMode = YES;
+    [self initializeStatusBarItem];
     
-    NSStatusBarButton *button = self.statusItem.button;
-//    button.image = [NSImage imageNamed:@"purple@2x.png"];
-    button.image = [TestImage imageOfMyImage:23];
-    button.action = @selector(buttonClicked2:);
     NSString *portTitle = @"Preferencees";
     NSString *quitTitle = @"Quit";
     self.statusItem.menu = [self initializeStatusBarMenu:@{
        portTitle: [NSValue valueWithPointer:nil],
        quitTitle: [NSValue valueWithPointer:@selector(terminate:)]
        }];
-}
-
-- (IBAction)buttonClicked2:(NSStatusBarButton *)sender
-{
-    NSLog(@"bar");
-}
-
-//- (IBAction)buttonClicked:(NSStatusBarButton *)sender
-//{
-//    // Create view controller
-//    PopoverViewController *viewController = [[PopoverViewController alloc] init];
-//    
-//    // Create popover
-//    NSPopover *entryPopover = [[NSPopover alloc] init];
-//    [entryPopover setContentSize:NSMakeSize(200.0, 200.0)];
-//    [entryPopover setBehavior:NSPopoverBehaviorTransient];
-//    [entryPopover setAnimates:YES];
-//    [entryPopover setContentViewController:viewController];
-//    
-//    // Convert point to main window coordinates
-////    NSRect entryRect = [sender convertRect:sender.bounds
-////                                    toView:self.statusItem.view];
-//    
-//    // Show popover
-//    [entryPopover showRelativeToRect:sender.bounds
-//                              ofView:self.statusItem.view
-//                       preferredEdge:NSMinYEdge];
-//    
-//    // handle close click
-//    if (self.popoverTransiencyMonitor == nil) {
-//        self.popoverTransiencyMonitor = [NSEvent addGlobalMonitorForEventsMatchingMask:(NSEventMaskLeftMouseDown | NSEventMaskRightMouseDown | NSEventMaskKeyUp) handler:^(NSEvent* event) {
-//                [NSEvent removeMonitor:self.popoverTransiencyMonitor];
-//                self.popoverTransiencyMonitor = nil;
-//                [entryPopover close];
-//        }];
-//    }
-//}
-
--(void)applicationWillTerminate:(NSNotification *)aNotification {
     
-    [[NSStatusBar systemStatusBar] removeStatusItem:self.statusItem];
-    self.statusItem = nil;
+    // first call
+    [self performSelector:@selector(timerFired:) withObject:nil];
 }
 
-
-- (NSStatusItem *)initializeStatusBarItem {
-    NSStatusItem *statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    statusItem.alternateImage = [NSImage imageNamed:@"white_alt@2x.png"];
-    statusItem.highlightMode = YES;
-    return statusItem;
+- (void)timerFired:(NSTimer *)timer
+{
+    NSString *dataUrl = @"https://feed.aqicn.org/xservices/refresh:1284?b6928d68172703fe9468ea70e38a330439c3e1a2";
+    NSURL *url = [NSURL URLWithString:dataUrl];
+    
+    NSURLSessionDataTask *downloadTask = [[NSURLSession sharedSession]
+                                          dataTaskWithURL:url
+                                          completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                              NSDictionary *jsonData = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                              NSString *measurement = jsonData[@"aqiv"];
+                                              int reading = [measurement intValue];
+                                              [self updateStatusItemWithReading:reading];
+                                              NSLog(@"%@", jsonData);
+                                          }];
+    
+    // 3
+    [downloadTask resume];
+    
+    
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:theRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//        jsonData = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+//        NSLog(@"%@", json);
+//    }];
+//
+//    [dataTask resume];
 }
 
--(NSMenu *)initializeStatusBarMenu:(NSDictionary*)menuDictionary {
+- (void)updateStatusItemWithReading:(int)reading
+{
+    NSImage *image = [TestImage imageOfMyImage:reading];
+    [self.statusItem setImage:image];
+}
+
+- (void)initializeStatusBarItem {
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    self.statusItem.highlightMode = YES;
+    NSStatusBarButton *button = self.statusItem.button;
+    button.action = @selector(buttonClicked2:);
+}
+
+- (NSMenu *)initializeStatusBarMenu:(NSDictionary *)menuDictionary {
     NSMenu *menu = [[NSMenu alloc] init];
     
     [menuDictionary enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSValue *val, BOOL *stop) {
@@ -112,5 +94,47 @@
     
     return menu;
 }
+
+- (IBAction)buttonClicked2:(NSStatusBarButton *)sender
+{
+    NSLog(@"bar"); // never called because menu implemented for statusItem
+}
+
+-(void)applicationWillTerminate:(NSNotification *)aNotification {
+    
+    [[NSStatusBar systemStatusBar] removeStatusItem:self.statusItem];
+    self.statusItem = nil;
+}
+
+//- (IBAction)buttonClicked:(NSStatusBarButton *)sender
+//{
+//    // Create view controller
+//    PopoverViewController *viewController = [[PopoverViewController alloc] init];
+//
+//    // Create popover
+//    NSPopover *entryPopover = [[NSPopover alloc] init];
+//    [entryPopover setContentSize:NSMakeSize(200.0, 200.0)];
+//    [entryPopover setBehavior:NSPopoverBehaviorTransient];
+//    [entryPopover setAnimates:YES];
+//    [entryPopover setContentViewController:viewController];
+//
+//    // Convert point to main window coordinates
+////    NSRect entryRect = [sender convertRect:sender.bounds
+////                                    toView:self.statusItem.view];
+//
+//    // Show popover
+//    [entryPopover showRelativeToRect:sender.bounds
+//                              ofView:self.statusItem.view
+//                       preferredEdge:NSMinYEdge];
+//
+//    // handle close click
+//    if (self.popoverTransiencyMonitor == nil) {
+//        self.popoverTransiencyMonitor = [NSEvent addGlobalMonitorForEventsMatchingMask:(NSEventMaskLeftMouseDown | NSEventMaskRightMouseDown | NSEventMaskKeyUp) handler:^(NSEvent* event) {
+//                [NSEvent removeMonitor:self.popoverTransiencyMonitor];
+//                self.popoverTransiencyMonitor = nil;
+//                [entryPopover close];
+//        }];
+//    }
+//}
 
 @end
